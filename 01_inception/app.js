3d320401d035bdb8436042a9036ca8d24f12e4dc
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-25 15:52:52 +0530

    EP1 part4

--- 01_inception/app.js
+++ 01_inception/app.js
@@ -1,9 +1,30 @@
-const heading = React.createElement(
-    "h1",
+const parent = React.createElement(
+    "div",
     {
-        id: "heading",
+        id: "parent",
     },
-    "Hello world!"
+    React.createElement(
+        "div",
+        {
+            id: "child",
+        },
+        [
+            React.createElement(
+                "h1",
+                {
+                    id: "heading",
+                },
+                "Hello world!"
+            ),
+            React.createElement(
+                "h2",
+                {
+                    id: "heading",
+                },
+                "Hello world! from h2"
+            ),
+        ]
+    )
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
